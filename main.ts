bebb56d36ccf6a7941d71c7b3f0e5f3d48116a7c
ouadiilyes3-sketch <ouadiilyes3@gmail.com>
2026-09-22 13:03:01 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+enum RadioMessage {
+    message1 = 49434
+}
+music.play(music.stringPlayable("C5 A F D C E G B ", 500), music.PlaybackMode.LoopingInBackground)
+basic.forever(function () {
+    basic.showString("ilyes")
+})
